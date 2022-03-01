bba438c1c6f6ad7cb15403c2648a7e2b502bc3f9
JD W <97631659+jwat36@users.noreply.github.com>
2022-03-01 14:02:32 -0800

    Add files via upload

---
+++ SodokuSolver/SodokuSolver/Workers/SudokuFileReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SudokuSolver.Workers
+{
+    internal class SudokuFileReader
+    {
+        public int [,] ReadFile (string filename)
+        {
+            int[,] sudokuBoard = new int[9, 9];
+            try
+            {
+                var sudokuBoardLines = File.ReadAllLines(filename);
+                int row = 0;
+                foreach (var sudokuBoardLine in sudokuBoardLines)
+                {
+                    string[] sudokuBoardLineElements = sudokuBoardLine.Split('|').Skip(1).Take(9).ToArray();
+
+                    int column = 0;
+                    foreach (var sudokuBoardLineElement in sudokuBoardLineElements)
+                    {
+                        sudokuBoard[row, column] = sudokuBoardLineElement.Equals(" ") ? 0 : Convert.ToInt16(sudokuBoardLineElement);
+                        column++;
+                    }
+                    row++;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("Something went wrong.  Please check the file and try again");
+            }
+            return sudokuBoard;
+        }
+    }
+}
